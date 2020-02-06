0aca3ba9f50475f2e57b3ddc722553d904ed97eb
Matthew Courchaine <mdcourchaine@gmail.com>
2020-02-06 19:41:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" ", " ", " ", " ", "x", " ", " ", " ", " "]
+ def display_board
+   puts "#{board[0]} | #{board[1]} | #{board[2]}"
+   puts "___________"
+   puts "#{board[3]} | #{board[4]} | #{board[5]}"
+   puts "___________"
+   puts "#{board[6]} | #{board[7]} | #{board[8]}"
+ end
+ 
+  
